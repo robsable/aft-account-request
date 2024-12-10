194eb17f53b281705e68299308e489e0394e96e7
robsable <54452843+robsable@users.noreply.github.com>
2024-12-10 11:52:46 -0500

    Add files via upload

---
+++ terraform/account-imports.tf
@@ -0,0 +1,26 @@
+module "account_import_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "robsable+saa@amazon.com"
+    AccountName               = "Gen AI Dev"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "robsable+aft@amazon.com"
+    SSOUserFirstName          = "Rob"
+    SSOUserLastName           = "Sable"
+  }
+
+  account_tags = {
+    "RobSable:Owner"       = "robsable@amazon.com"
+    "RobSable:Environment" = "SANDBOX"
+    "RobSable:CostCenter"  = "123456"
+    "RobSable:Vended"      = "true"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Automation"
+    change_reason       = "AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
