f8b099f939400db0bf161e1bbe2d6068ef895143
robsable <54452843+robsable@users.noreply.github.com>
2024-12-10 14:35:08 -0500

    Update account-imports.tf

--- terraform/account-imports.tf
+++ terraform/account-imports.tf
@@ -24,3 +24,30 @@ module "account_import_01" {
 
   account_customizations_name = "SANDBOX"
 }
+
+module "account_import_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "robsable+focal@amazon.com"
+    AccountName               = "Focalboard"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "robsable+aft@amazon.com"
+    SSOUserFirstName          = "Rob"
+    SSOUserLastName           = "Sable"
+  }
+
+  account_tags = {
+    "RobSable:Owner"       = "robsable@amazon.com"
+    "RobSable:Environment" = "SANDBOX"
+    "RobSable:CostCenter"  = "123456"
+    "RobSable:Vended"      = "true"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Automation"
+    change_reason       = "AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
